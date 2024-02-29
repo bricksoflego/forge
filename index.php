bdbf39a4d42bc7dcd0de98ca70742d90c32ca050
Brent Ricks <brent.ricks@microsoft.com>
2024-02-29 15:21:10 -0700

    PHP

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+	This is just a blank site for furture Forge.com
+</body>
+</html>
