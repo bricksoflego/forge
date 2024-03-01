5da3036f86c5cb40c7c22ff448a8315fa0da7ed3
Brent Ricks <brent.ricks@microsoft.com>
2024-03-01 13:58:33 -0700

    Font references Fixed

--- index.php
+++ index.php
@@ -7,8 +7,6 @@
 <title>Forge - Home</title>
 <link rel="stylesheet" href="Content/Scripts/site.css" />
 <link rel="icon" type="image/x-icon" href="mLogo.ico" />
-<link rel="preload" href="Content/Fonts/CCBiffBamBoomOutline.woff2" as="font" type="font/woff2" crossorigin>
-<link rel="preload" href="Content/Fonts/ClassicComic-Medium.woff2" as="font" type="font/woff2" crossorigin>
 </head>
 <body>
 	<header><img src="Content/Images/mLogo.png" /></header>
