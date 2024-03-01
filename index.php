e1b05332cb39bfdfce270c54d8106b3cdd1c5dc0
Brent Ricks <brent.ricks@microsoft.com>
2024-03-01 13:47:34 -0700

    More Template Fun

--- index.php
+++ index.php
@@ -7,12 +7,24 @@
 <title>Forge - Home</title>
 <link rel="stylesheet" href="Content/Scripts/site.css" />
 <link rel="icon" type="image/x-icon" href="mLogo.ico" />
+<link rel="preload" href="Content/Fonts/CCBiffBamBoomOutline.woff2" as="font" type="font/woff2" crossorigin>
+<link rel="preload" href="Content/Fonts/ClassicComic-Medium.woff2" as="font" type="font/woff2" crossorigin>
 </head>
 <body>
 	<header><img src="Content/Images/mLogo.png" /></header>
 	<main>
-		This is just a blank site for furture Forge.com<br/>
-		<? echo(date("m/d/y")) ?>
+		<div id="divWelcome">
+			<h2>Welcome to the</h2>
+			<h1>Forge</h1>
+		</div>
+		<hr />
+		<div class="divDate">
+			<strong>Today is:</strong> 
+			<? 
+				$today = new DateTime();
+				echo $today->format('F d, Y') 
+			?>
+		</div>
 	</main>
 </body>
 </html>
