7f7ce88ec6bb1da3b096ee0b6f08f320c880f205
Brent Ricks <brent.ricks@microsoft.com>
2024-02-29 23:10:24 -0700

    Added Color

--- index.php
+++ index.php
@@ -6,10 +6,13 @@
 <meta charset="utf-8">
 <title>Forge - Home</title>
 <link rel="stylesheet" href="Content/Scripts/site.css" />
+<link rel="icon" type="image/x-icon" href="mLogo.ico" />
 </head>
-<header></header>
 <body>
-	This is just a blank site for furture Forge.com<br/>
-	<? echo(date("m/d/y")) ?>
+	<header><img src="Content/Images/mLogo.png" /></header>
+	<main>
+		This is just a blank site for furture Forge.com<br/>
+		<? echo(date("m/d/y")) ?>
+	</main>
 </body>
 </html>
